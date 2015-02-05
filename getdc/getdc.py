44eeec31d55f80965d90d823fad09dc4be51c1b7
Alan Viars <alan.c.viars@gmail.com>
2015-02-05 12:24:42 -0500

    Added functions to get certs as string. Added DNS timeout error handling.  Misc bugfixes

--- getdc/getdc.py
+++ getdc/getdc.py
@@ -9,7 +9,59 @@ import base64
 import dns.resolver
 import ldap
 
-def get_certificate(endpoint):
+def get_certificate(endpoint, save_to_disk= False, file_extension="pem"):
+    response = ""
+    if endpoint.__contains__("@"):
+        email_endpoint = True
+        email_username, email_domain = endpoint.split("@", 1)
+    else:
+        email_endpoint = False
+    
+    if email_endpoint:
+        email_domain_bound_dns  = get_certificate_dns(email_domain, save_to_disk, file_extension)
+        email_domain_bound_ldap = get_certificate_ldap(email_domain, save_to_disk, file_extension)
+      
+    
+        if email_domain_bound_dns.startswith("-----BEGIN CERTIFICATE-----"):
+            response = response + email_domain_bound_dns
+        
+        elif email_domain_bound_ldap.startswith("-----BEGIN CERTIFICATE-----"):
+            response = response + email_domain_bound_ldap
+                         
+        else:
+            #Try it a 2nd way, and try to get the email-bound certificate.
+            endpoint_email_bound_dns  = get_certificate_dns(endpoint, save_to_disk, file_extension)
+            endpoint_email_bound_ldap = get_certificate_ldap(endpoint, save_to_disk, file_extension)
+            if endpoint_email_bound_dns.startswith("-----BEGIN CERTIFICATE-----"):
+                response = response + endpoint_email_bound_dns
+                
+            elif endpoint_email_bound_ldap.startswith("-----BEGIN CERTIFICATE-----"):
+                response = response + endpoint_email_bound_ldap
+            else:
+                response = "No certificate was found via LDAP or DNS."
+            
+    else:
+        # Appears to be domain only     
+    
+        dns_response  = get_certificate_dns(endpoint,  save_to_disk, file_extension)
+        ldap_response = get_certificate_ldap(endpoint,  save_to_disk, file_extension)        
+        
+        if dns_response.startswith("-----BEGIN CERTIFICATE-----"):
+            response = response + dns_response
+        
+        if ldap_response.startswith("-----BEGIN CERTIFICATE-----"):
+            response = response + ldap_response
+
+    if not response:
+        response = "No certificate was found via LDAP or DNS."
+    
+    return response
+   
+   
+
+
+
+def validate_certificate(endpoint, download_certificate=False):
     result = {} 
     if endpoint.__contains__("@"):
         email_endpoint = True
@@ -18,8 +70,8 @@ def get_certificate(endpoint):
         email_endpoint = False
     
     if email_endpoint:
-        email_domain_bound_dns  = get_certificate_dns(email_domain)
-        email_domain_bound_ldap = get_certificate_ldap(email_domain)
+        email_domain_bound_dns  = validate_certificate_dns(email_domain, download_certificate)
+        email_domain_bound_ldap = validate_certificate_ldap(email_domain, download_certificate)
       
     
         if email_domain_bound_dns['is_found'] or email_domain_bound_ldap['is_found']:
@@ -30,8 +82,8 @@ def get_certificate(endpoint):
              
         else:
             #Try it a 2nd way, and try to get the email-bound certificate.
-            endpoint_email_bound_dns  = get_certificate_dns(endpoint)
-            endpoint_email_bound_ldap = get_certificate_ldap(endpoint)
+            endpoint_email_bound_dns  = validate_certificate_dns(endpoint, download_certificate)
+            endpoint_email_bound_ldap = validate_certificate_ldap(endpoint, download_certificate)
             if endpoint_email_bound_dns['is_found'] or endpoint_email_bound_ldap['is_found']:
                 result['email_bound_cert'] = True
                 result['is_found']         = True
@@ -45,8 +97,8 @@ def get_certificate(endpoint):
     else:
         # Appears to be domain only     
     
-        result['dns']  = get_certificate_dns(endpoint)
-        result['ldap'] = get_certificate_ldap(endpoint)        
+        result['dns']  = validate_certificate_dns(endpoint, download_certificate)
+        result['ldap'] = validate_certificate_ldap(endpoint, download_certificate)        
         
         if result['dns']['is_found'] or result['ldap']['is_found']:
             result['is_found']=True
@@ -55,7 +107,7 @@ def get_certificate(endpoint):
     return result    
 
     
-def get_certificate_dns(endpoint, response={"is_found":False}):
+def validate_certificate_dns(endpoint, download_certificate=False, response={"is_found":False}):
         
         endpoint =  endpoint.replace("@", ".")
         
@@ -63,17 +115,18 @@ def get_certificate_dns(endpoint, response={"is_found":False}):
             answers = dns.resolver.query(endpoint, 'CERT')
             i=1
             for rdata in answers:
-                if i > 1:
+                if download_certificate:
+                    if i > 1:
                         fn = "%s_%s.pem" % (endpoint, i)
-                else:
+                    else:
                         fn = "%s.pem" % (endpoint)
-                fh = open(fn, "w") 
-                fh.writelines("-----BEGIN CERTIFICATE-----\n")
-                fh.writelines(base64.encodestring(rdata.certificate).rstrip())
-                fh.writelines("\n-----END CERTIFICATE-----\n")
-                fh.close()
-                i+=1 
-            msg = "certificate %s found." % (endpoint)
+                    fh = open(fn, "w") 
+                    fh.writelines("-----BEGIN CERTIFICATE-----\n")
+                    fh.writelines(base64.encodestring(rdata.certificate).rstrip())
+                    fh.writelines("\n-----END CERTIFICATE-----\n")
+                    fh.close()
+                    i+=1 
+            msg = "The certificate %s was found." % (endpoint)
             response.update({"status": 200, "message": msg,
                              "is_found": True})
                 
@@ -82,17 +135,21 @@ def get_certificate_dns(endpoint, response={"is_found":False}):
                              "details" : "No DNS server found."})
         
         except dns.resolver.NoNameservers:
-            response.update({"status": 412, "message": "Network failure. No certificate found.",
-                        "details": "You may be disconnected from the Internet. If you have an Internet connection then it is likely that a certificate exists, but or your intetrnet service provider (ISP) blocks large DNS requests. Many ISPs do this block including Time Warner Cable and Frontier Cable."
+            response.update({"status": 412, "message": "No Name Servers. Network failure. No certificate found.",
+                        "details": "You may be disconnected from the Internet. If you have an Internet connection then a certificate may exist, but or your intetrnet service provider (ISP) blocks large DNS requests. Many ISPs do this block including Time Warner Cable and Frontier Cable."
                         })
             
         except dns.resolver.NoAnswer:
-            response.update({"status": 404, "message": "The server did not provide an answer. No certificate found."})
-            
+            response.update({"status": 404, "message": "No Answer.",
+                             "details":"The server did not provide an answer. No certificate found."})
+        
+        except dns.exception.Timeout:
+             response.update({"status": 500, "message": "Timeout",
+                "details": "The certifcate may exist but or your intetrnet service provider (ISP) blocks large DNS requests. Many ISPs do this block including Time Warner Cable and Frontier Cable."})
         return response   
             
-def get_certificate_ldap(endpoint, response={"is_found":False}):
-    
+def validate_certificate_ldap(endpoint, download_certificate=False, response={"is_found":False}):
+    error = False
     endpoint =  endpoint.replace("@", ".")   
     
     try:
@@ -106,8 +163,8 @@ def get_certificate_ldap(endpoint, response={"is_found":False}):
 
     except dns.resolver.NXDOMAIN:
         response.update({"status": 404, "message": "No certificate found.",
-                    "details" : "No LDAP server found.","is_found":False  })
-        error=Truee
+                    "details" : "No LDAP server was found.","is_found":False  })
+        error=True
 
     except dns.resolver.NoAnswer:
         response.update( {"status": 404, "message": "No certificate found.",
@@ -144,34 +201,154 @@ def get_certificate_ldap(endpoint, response={"is_found":False}):
     i = 1
     
     for c in cert_ders:
-        
-        if i > 1:
-            fn = "%s_%s.pem" % (endpoint, i)
-        else:
-            fn = "%s.pem" % (endpoint)    
+        if download_certificate:
+            if i > 1:
+                fn = "%s_%s.pem" % (endpoint, i)
+            else:
+                fn = "%s.pem" % (endpoint)    
             
-        fh = open(fn, "w") 
-        fh.writelines("-----BEGIN CERTIFICATE-----\n")
-        fh.writelines(base64.encodestring(c).rstrip())
-        fh.writelines("\n-----END CERTIFICATE-----\n")
-        fh.close()
-        i += 1
+            fh = open(fn, "w") 
+            fh.writelines("-----BEGIN CERTIFICATE-----\n")
+            fh.writelines(base64.encodestring(c).rstrip())
+            fh.writelines("\n-----END CERTIFICATE-----\n")
+            fh.close()
+            i += 1
 
-    msg = "certificate %s found." % (endpoint)
+    msg = "The certificate %s was found." % (endpoint)
     response.update({"status": 200, "message": msg, "is_found": True})    
         
     return response   
 
+
+
+def get_certificate_dns(endpoint,  save_to_disk= True, file_extension="pem"):
+        response =""
+        endpoint =  endpoint.replace("@", ".")
+        
+        try:
+            answers = dns.resolver.query(endpoint, 'CERT')
+            i=1
+            for rdata in answers:
+                if save_to_disk:
+                    
+                    if i > 1:
+                        fn = "%s_%s.%s" % (endpoint, i, file_extension)
+                    else:
+                        fn = "%s.%s" % (endpoint, file_extension)
+                    fh = open(fn, "w")
+                    fh.writelines("-----BEGIN CERTIFICATE-----\n")
+                    fh.writelines(base64.encodestring(rdata.certificate).rstrip())
+                    fh.writelines("\n-----END CERTIFICATE-----\n")
+                    fh.close()
+                    i+=1
+                response = response + "-----BEGIN CERTIFICATE-----\n" + \
+                   base64.encodestring(rdata.certificate).rstrip() + \
+                   "\n-----END CERTIFICATE-----\n"
+            return response
+                
+        except dns.resolver.NXDOMAIN:
+            response ="No DNS server found. No certificate found."
+        
+        except dns.resolver.NoNameservers:
+            response = "No nameservers. Network failure. No certificate found. You may be disconnected from the Internet. If you have an Internet connection then a certificate may exist, but or your Intetrnet Service Provider (ISP) blocks large DNS requests. Many ISPs do this block including Time Warner Cable and Frontier Cable."    
+            
+        except dns.resolver.NoAnswer:
+            response = "No Answer. The server did not provide an answer. No certificate found."
+        
+        except dns.exception.Timeout:
+             response="Timeout. A certificate may exist, but or your Intetrnet service provider (ISP) blocks large DNS requests. Many ISPs do this block including Time Warner Cable and Frontier Cable."
+        return response 
+
+
+
+def get_certificate_ldap(endpoint, save_to_disk= True, file_extension="pem"):
+    response =""
+    endpoint =  endpoint.replace("@", ".")   
+    
+    try:
+        ldap_servers = dns.resolver.query("_ldap._tcp."+endpoint, 'SRV').response.answer[0].items
+        error=False
+    except dns.resolver.NoNameservers:
+        response ="Network failure. You appear to be disconnected from the Internet."
+        error=True
+
+    except dns.resolver.NXDOMAIN:
+        response ="No certificate found. No LDAP server was found."
+        error=True
+
+    except dns.resolver.NoAnswer:
+        response ="No certificate found. The server did not provide an answer."
+        error=True
    
+    if error:
+        return response
+        
+        
+    servers = [{
+       'port': s.port, 
+       'priority': s.priority,
+       'host': s.target.to_text()
+       } for s in ldap_servers]
+        
+    ldap_results = []
+        
+    for s in servers:
+        url = "ldap://%(host)s:%(port)s"%s
+        l = ldap.initialize(url)
+        result_id  = l.search("", ldap.SCOPE_SUBTREE, "mail=%s"%endpoint, None)
+        while True:
+            rtype, rdata = l.result(result_id, 0)
+            if rdata == []: break
+            ldap_results.append((rtype, rdata))
+
+    # Only take valid results
+    ldap_results = filter(lambda r: r[0] == 100, ldap_results)
+
+    # Extract binary (DER) certs from responses
+    cert_ders = ["".join(r[1][0][1]['userCertificate']) for r in ldap_results]      
+    i = 1
+    
+    for c in cert_ders:
+        if  save_to_disk: 
+            if i > 1:
+                fn = "%s_%s.%s" % (endpoint, i, file_extension)
+            else:
+                fn = "%s.%s" % (endpoint, file_extension)    
+          
+            fh = open(fn, "w") 
+            fh.writelines("-----BEGIN CERTIFICATE-----\n")
+            fh.writelines(base64.encodestring(c).rstrip())
+            fh.writelines("\n-----END CERTIFICATE-----\n")
+            fh.close()
+        response = response + "-----BEGIN CERTIFICATE-----\n" + \
+                   base64.encodestring(c).rstrip() + \
+                   "\n-----END CERTIFICATE-----\n"
+        i += 1
+        
+    return response
+
+
+
+
+
+
 if __name__ == "__main__": 
     
     #Get the file from the command line
-    if len(sys.argv)<2:
-        print "You must suppy an email or endpoint."
+    if len(sys.argv)<3:
+        print "You must suppy an endpoint and indicate wheather or not you want to download the certificate."
         print "For example, jon@direct.example.com or direct.example.com"
-        print "Usage: get_certificate_dns [email/endpoint]"
+        print "Usage: get_certificate_dns [email/endpoint] [Download_Certificate Y/N]"
         sys.exit(1)
     else:
         endpoint = sys.argv[1]
-        result = get_certificate(endpoint)
+        if sys.argv[2] in ("Y", "y", "yes", "t", "T", "true", "True"):
+            download_certificate = True
+        else:
+             download_certificate = False   
+        
+        result = validate_certificate(endpoint, download_certificate)
         print json.dumps(result, indent=4)
+
+        
+        
