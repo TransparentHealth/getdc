60e9f7573850a0db3d765ccdf6ee31dee832a182
Alan Viars <alan.c.viars@gmail.com>
2015-11-17 10:40:03 -0500

    reset versions

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
 
 
 setup(name="getdc",
-      version="0.9.9.1",
+      version="0.1.1.2",
       description="Library of tools for fetching and parsing x509 certificate from DNS and LDAP",
       long_description="""Outputs a JSON object of success or errors. 
           Requires dnspython and python-ldap to be installed and python-dev libldap2-dev libsasl2-dev need to be installed 
