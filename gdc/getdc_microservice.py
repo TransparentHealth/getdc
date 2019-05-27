49ca35735af53a06aa951cd9bcca06dd66326349
Alan Viars <aviars@videntity.com>
2019-05-27 14:36:47 -0400

    Adding Python 2 and 3 support

--- gdc/getdc_microservice.py
+++ gdc/getdc_microservice.py
@@ -1,13 +1,22 @@
-from BaseHTTPServer import BaseHTTPRequestHandler
+#!/usr/bin/env python
+# Written by Alan Viars
 import json
+import sys
 from validate_email import validate_email
 from collections import OrderedDict
 from get_direct_certificate import DCert
 
-__author__ = "Alan Viars"
+if (sys.version_info >= (3, 0)):
+    # Python 3 code in this block
+    from http.server import BaseHTTPRequestHandler, HTTPServer
 
-PORT = 8080
-IP = "0.0.0.0"
+else:
+    # Python 2 code in this block
+    from BaseHTTPServer import BaseHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+
+
+__author__ = "Alan Viars"
 
 
 class GetHandler(BaseHTTPRequestHandler):
@@ -30,15 +39,26 @@ class GetHandler(BaseHTTPRequestHandler):
         dc = DCert(response['endpoint'])
         dc.validate_certificate(False)
         response["direct_address"] = dc.result['is_found']
-
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(json.dumps(response, indent=2))
+        j = json.dumps(response, indent=2).encode('utf8')
+        self.wfile.write(j)
         return
 
 
 if __name__ == '__main__':
-    from BaseHTTPServer import HTTPServer
-    server = HTTPServer((IP, PORT), GetHandler)
-    print('Starting server at http://%s:%s' % (IP, PORT))
-    server.serve_forever()
+
+    if len(sys.argv) is not 3:
+        print("You must supply a host/ip and a port.")
+        print("Usage: python getdc_microservice.py [HOST-or-IP] [PORT]")
+        print("Example: python getdc_microservice.py 0.0.0.0 8888")
+        sys.exit(1)
+    else:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        server = HTTPServer((IP, PORT), GetHandler)
+
+        print('Starting Certificate Discovery server at http://%s:%s' % (IP, PORT))
+        server.serve_forever()
+
+    # Get the file from the command line
