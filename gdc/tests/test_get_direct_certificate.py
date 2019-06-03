1f42ce791552a5f4f132c324171e2426133065fb
Alan Viars <aviars@videntity.com>
2019-06-03 13:37:06 -0400

    Adding some tests.

---
+++ gdc/tests/test_get_direct_certificate.py
@@ -0,0 +1,32 @@
+import unittest
+from gdc.get_direct_certificate import DCert
+
+__author__ = "Alan Viars"
+
+"""Please note the DNS test will fail if your ISP blocks large DNS responses.
+This includes Time Warner Cable and Frontier Cable."""
+
+
+class TestGetDirectCertificate(unittest.TestCase):
+
+    def test_no_cert_found(self):
+        endpoint = "direct.example.com"
+        dc = DCert(endpoint)
+        dc.validate_certificate(download_certificate=False)
+        self.assertFalse(dc.result['is_found'])
+
+    def test_dns_cert_found(self):
+        endpoint = "ett.healthit.gov"
+        dc = DCert(endpoint)
+        dc.validate_certificate(download_certificate=False)
+        self.assertTrue(dc.result['is_found'])
+        self.assertTrue(dc.result['dns']['is_found'])
+
+    def test_ldap_cert_found(self):
+        endpoint = "d4@domain2.dcdt31prod.sitenv.org"
+        dc = DCert(endpoint)
+        dc.validate_certificate(download_certificate=False)
+        self.assertTrue(dc.result['ldap']['is_found'])
+
+if __name__ == '__main__':
+    unittest.main()
